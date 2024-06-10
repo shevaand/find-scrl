3b3577b3c774a49e44bf2377f40becec76f72fbd
Andrew Shevchenko <bartsimpsonpvp@gmail.com>
2024-06-10 12:42:15 +0300

    fast fix

--- src/components/Body/BodyEx.js
+++ src/components/Body/BodyEx.js
@@ -1,12 +1,8 @@
 import './body.css'
-import image from '../img/1.png'
-import image2 from '../img/11.jpeg'
 
 function Body () {
   return (
-    <div>
-      <img src={image} />
-      <img src={image2} />
+    <div className="background-container">
     </div>
   )
 }
